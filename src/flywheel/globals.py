9dd144e78bde097c8c1e2904f14ca82e862f27cc
Elaina <GreyElaina@outlook.com>
2024-03-16 21:07:40 +0800

    chore: format change

--- src/flywheel/globals.py
+++ src/flywheel/globals.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import functools
 from collections import defaultdict
 from contextvars import ContextVar, copy_context
-from typing import Any, Callable, Generator
+from typing import Any, Callable, Generator, Tuple
 
 from .context import CollectContext, InstanceContext
 from .typing import P, R, TEntity
@@ -12,7 +12,7 @@ GLOBAL_COLLECT_CONTEXT = CollectContext()
 GLOBAL_INSTANCE_CONTEXT = InstanceContext()
 
 COLLECTING_CONTEXT_VAR = ContextVar("CollectingContext", default=GLOBAL_COLLECT_CONTEXT)
-LOOKUP_LAYOUT_VAR: ContextVar[tuple[CollectContext, ...]] = ContextVar("LookupContext", default=(GLOBAL_COLLECT_CONTEXT,))
+LOOKUP_LAYOUT_VAR = ContextVar[Tuple[CollectContext, ...]]("LookupContext", default=(GLOBAL_COLLECT_CONTEXT,))
 INSTANCE_CONTEXT_VAR = ContextVar("InstanceContext", default=GLOBAL_INSTANCE_CONTEXT)
 
 ITER_BUCKET_VAR: ContextVar[defaultdict[Any, list[int]]] = ContextVar("LAYOUT_ITER_COLLECTIONS")

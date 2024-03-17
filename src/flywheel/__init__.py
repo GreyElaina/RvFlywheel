6c42f881a7da8b41ae3640ead4f0dbc924a66144
Elaina <GreyElaina@outlook.com>
2024-03-17 22:13:50 +0800

    feat: import entrypoint

---
+++ src/flywheel/__init__.py
@@ -0,0 +1,15 @@
+from .context import CollectContext as CollectContext
+from .context import InstanceContext as InstanceContext
+from .fn import Fn as Fn
+from .fn import FnCompose as FnCompose
+from .fn import FnImplement as FnImplement
+from .fn import FnImplementEntity as FnImplementEntity
+from .fn import FnOverload as FnOverload
+from .fn import FnRecord as FnRecord
+from .fn import OverloadRecorder as OverloadRecorder
+from .globals import global_collect as global_collect
+from .globals import local_collect as local_collect
+from .instance_of import InstanceOf as InstanceOf
+from .overloads import SimpleOverload as SimpleOverload
+from .overloads import TypeOverload as TypeOverload
+from .scoped import scoped_collect as scoped_collect
---
+++ src/flywheel/fn/__init__.py
@@ -0,0 +1,7 @@
+from .base import Fn as Fn
+from .compose import FnCompose as FnCompose
+from .implement import FnImplementEntity as FnImplementEntity
+from .implement import OverloadRecorder as OverloadRecorder
+from .overload import FnOverload as FnOverload
+from .record import FnImplement as FnImplement
+from .record import FnRecord as FnRecord
